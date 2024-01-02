370f01c60935a5142e6dfb44f9b09308d737191c
Aerine <ry7arin@gmail.com>
2024-01-03 01:02:20 +0900

    ゲッターセッター追加

--- src/main/java/com/github/aer0119/traveltool/domain/EventContent.java
+++ src/main/java/com/github/aer0119/traveltool/domain/EventContent.java
@@ -5,9 +5,44 @@ import java.util.UUID;
 
 public class EventContent {
     private UUID eventPlanItemId;
-    private String planName;
+    private String contentName;
     private String description;
     private Date startDate;
     private Date endDate;
 
+    public UUID getEventPlanItemId() {
+        return eventPlanItemId;
+    }
+
+    public String getPlanName() {
+        return contentName;
+    }
+
+    public void setPlanName(String contentName) {
+        this.contentName = contentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
--- src/main/java/com/github/aer0119/traveltool/domain/EventPlan.java
+++ src/main/java/com/github/aer0119/traveltool/domain/EventPlan.java
@@ -11,4 +11,44 @@ public class EventPlan {
     private Date startDate;
     private Date endDate;
     private ArrayList<EventContent> eventContents;
+
+
+    public UUID getEventPlanId(){
+        return eventPlanId;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+    public Date getStartDate(){
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate){
+        this.startDate=startDate;
+    }
+
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
+
